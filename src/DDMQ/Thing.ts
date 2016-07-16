cf46288aa5dded43f7661612f03f5be615bea6d7
Hyeonjin <hyeonjin507@gmail.com>
2016-07-17 00:10:00 +0900

    change how to subscribe add underscore for utils.ts

--- src/DDMQ/Thing.ts
+++ src/DDMQ/Thing.ts
@@ -1,239 +1,130 @@
 import * as mqtt from 'mqtt';
 import { mkString } from './utils';
-//import { ThingSubscription } from './ThingsSubscription';
+import { Subscription, SubscribeTopicOptions } from './Subscription';
 
-export module Thing {
+/**
+ * Thing class
+ */
+export class Thing{
+
   /**
-   * Options for MQTT subscription for a topic
+   * Member Variables
    */
-  interface SubscribeTopicOptions{
-    collection? :string;
-    documentId? :string;
-    field?      :string;
-    event?      :string;
-    payload?    :string;
+  client   :mqtt.Client;
+
+  thingId  :string;
+  userName :string;
+
+  /**
+   * Constructor
+   */
+  constructor( thingId, userName, password, host ){
+
+    this.thingId  = thingId;
+    this.userName = userName;
+
+    this.client = mqtt.connect( host, {
+        clientId : thingId,
+        username : userName,
+        password : password
+    });
   }
 
   /**
-   * Class for DDMQ Subscription
-   *
-   * member methods:
-   *   onadded (func :Function)
-   *   onchanged (func :Function)
-   *   onremoved (func :Function)
-   *   onevent (ev :string, func :Function)
+   * a Function same with 'this.client.on'
+   * @return Thing
    */
-  export class Subscription{
-    client      :Client;
-    publishName :String;
+  on( event :string, listener :Function ) :Thing{
+    this.client.on( event, listener );
+    return this;
+  }
 
-    constructor( client :Client, publishName :string ){
-      this.client      = client;
-      this.publishName = publishName;
-    }
+  /**
+   * Function for MQTT subscription a publication
+   *
+   * @param {string} publishName - what this thing is subscribing
+   * @param {object} [optionsOrCallback] - options for subscribing or callback
+   * @param {function} [callback] - callback
+   * @return Thing.Subscription
+   * @example subscribeTopic('mPublishName');
+   * @example subscribeTopic('mPublishName', { event: 'added' });
+   */
+  subscribe( publishName :string, ...payload :any[] ) :Subscription{
+    if( typeof publishName !== 'string' )
+      throw new Error( 'publish name should be string!' );
 
-    /**
-     * Listener for Added event
-     * @param {Function} function
-     * @return Thing.Subscription
-     */
-    onadded( func :Function ) :Thing.Subscription{
-      this.onevent( 'added', func );
-      return this;
-    }
+    let callback = null;
+    if (payload && payload.length > 0 && typeof payload[payload.length-1] == 'function')
+      callback = payload.pop();
 
-    /**
-     * Listener for Changed event
-     * @param {Function} function
-     * @return Thing.Subscription
-     */
-    onchanged( func :Function ) :Thing.Subscription{
-      this.onevent( 'changed', func );
-      return this;
-    }
+    let thingId = this.thingId;
+    let topic   = `${ thingId }/${ publishName }/#`;
 
-    /**
-     * Listener for Removed event
-     * @param {Function} function
-     * @return Thing.Subscription
-     */
-    onremoved( func :Function ) :Thing.Subscription{
-      this.onevent( 'removed', func );
-      return this;
-    }
+    let subscription = new Subscription(this, publishName);
 
-    /**
-     * Listener for Added event
-     * @param {string} ev
-     * @param {Function} function
-     * @return Thing.Subscription
-     */
-    onevent( ev :string, func :Function ) :Thing.Subscription{
-      this.client.on('message', (topic, messageBuf) => {
-        let msg = messageBuf.toString();
-        let topicSplit = topic.split('/');
-
-        if ((topicSplit[1] === this.publishName || this.publishName == '+')
-         && topicSplit[4] === ev) {
-          func(topic, msg);
+    this.on('connect', () => {
+      this.client.subscribe( topic, (err, args) => {
+        // fired on $suback
+        // ex) args : [{ topic: 'myThing01/$inbox/#', qos: 0 }]
+        // (args[0].qos is 128 on error)
+        if (err || args[0].qos > 2){
+          console.log(`subscription "${topic}" failure : ${err}`);
+        }else{
+          this.DDMQSubscribe( publishName, mkString(payload), callback );
         }
       });
-      return this;
-    }
+    });
 
-    /**
-     * Listener for ANY event
-     * @param {Function} function
-     * @return Thing.Subscription
-     */
-    on( func :Function ) :Thing.Subscription{
-        this.client.on('message', (topic, messageBuf) => {
-          let msg = messageBuf.toString();
-          let topicSplit = topic.split('/');
-          /*
-          let publishNameSplit = this.publishName.split('/');
-
-          for ( let i=1; i<topic.split.length && this.publishName != '+'; i++ ) {
-            if ( topicSplit[i] != '+' && publishNameSplit[i-1] != '+'
-              && topicSplit[i] != publishNameSplit[i-1] ) return;
-          }
-          */
-
-          if ( topicSplit[1] === this.publishName
-            || this.publishName === '+') {
-
-            func(topic, msg);
-          }
-        });
-        return this;
-    }
+    return subscription;
   }
 
   /**
-   * Thing class
+   * Function for DDMQ Subscription
+   *
+   * @param {string} publishName - what this thing is subscribing
+   * @param {object} [options] - options for subscribing
+   * @param {function} [callback] - callback
+   * @return void
+   * @example DDMQSubscribeTopic('mPublishName');
+   * @example DDMQSubscribeTopic('mPublishName', { event: 'added' });
    */
-  export class Client{
+  private DDMQSubscribe( publishName :string, payload: string, callback? ) {
 
-    /**
-     * Member Variables
-     */
-    client   :mqtt.Client;
+    let thingId    = this.thingId;
+    let topic      = `${ thingId }/$sub/${ publishName }`;
 
-    thingId  :string;
-    userName :string;
-
-    /**
-     * Constructor
-     */
-    constructor( thingId, userName, password, host ){
-
-      this.thingId  = thingId;
-      this.userName = userName;
-
-      this.client = mqtt.connect( host, {
-          clientId : thingId,
-          username : userName,
-          password : password
-      });
-    }
-
-
-    /**
-     * a Function same with 'this.client.on'
-     * @return Thing.Client
-     */
-    on( event :string, listener :Function ) :Thing.Client{
-      this.client.on( event, listener );
-
-      return this;
-    }
-
-    /**
-     * Function for MQTT subscription a publication
-     *
-     * @param {string} publishName - what this thing is subscribing
-     * @param {object} [optionsOrCallback] - options for subscribing or callback
-     * @param {function} [callback] - callback
-     * @return Thing.Subscription
-     * @example subscribeTopic('mPublishName');
-     * @example subscribeTopic('mPublishName', { event: 'added' });
-     */
-    subscribe( publishName :string, ...payload :any[] ) :Thing.Subscription{
-      if( typeof publishName !== 'string' )
-        throw new Error( 'publish name should be string!' );
-
-      let callback = null;
-      if (payload && payload.length > 0 && typeof payload[payload.length-1] == 'function')
-        callback = payload.pop();
-
-      let thingId = this.thingId;
-      let topic   = `${ thingId }/${ publishName }/#`;
-
-      let subscription = new Thing.Subscription(this, publishName);
-
-      this.on('connect', () => {
-        this.client.subscribe( topic, (err, args) => {
-          // fired on $suback
-          // ex) args : [{ topic: 'myThing01/$inbox/#', qos: 0 }]
-          // (args[0].qos is 128 on error)
-          if (err || args[0].qos > 2)
-            console.log(`subscription "${topic}" failure : ${err}`);
-          else
-            this.DDMQSubscribe( publishName, mkString(payload), callback );
-        });
-      });
-
-      return subscription;
-    }
-
-    /**
-     * Function for DDMQ Subscription
-     *
-     * @param {string} publishName - what this thing is subscribing
-     * @param {object} [options] - options for subscribing
-     * @param {function} [callback] - callback
-     * @return void
-     * @example DDMQSubscribeTopic('mPublishName');
-     * @example DDMQSubscribeTopic('mPublishName', { event: 'added' });
-     */
-    private DDMQSubscribe( publishName :string, payload: string, callback? ) {
-
-      let thingId    = this.thingId;
-      let topic      = `${ thingId }/$sub/${ publishName }`;
-
-      this.client.publish( topic, payload, callback );
-    }
-
-    /**
-     * Function to MQTT unsubscribe
-     *
-     * @param {string} publishName - what this thing is unsubscribing
-     * @param {function} [callback] - callback
-     * @return void
-     */
-    unsubscribe( publishName :string, callback? :Function ) :void{
-      if( typeof publishName !== 'string' )
-        throw new Error( 'publish name should be string!' );
-
-      let thingId = this.thingId;
-      let topic   = `${ thingId }/${ publishName }/#`;
-
-      this.client.unsubscribe( topic, callback )
-    }
-
-   /**
-    * Function for MQTT publication
-    *
-    * @param {string} publishName - what this thing is publishing
-    * @param {string} payload - publication payload
-    * @return Thing.Client
-    * @example publish(`Hello`, `World! [${i++}]`);
-    */
-    publish(publishName :string, payload :string, callback? :Function) :Thing.Client{
-      this.client.publish(`${this.thingId}/${publishName}`, `${payload}`, callback);
-
-      return this;
-    }
-
+    this.client.publish( topic, payload, callback );
   }
+
+  /**
+   * Function to MQTT unsubscribe
+   *
+   * @param {string} publishName - what this thing is unsubscribing
+   * @param {function} [callback] - callback
+   * @return void
+   */
+  unsubscribe( publishName :string, callback? :Function ) :void{
+    if( typeof publishName !== 'string' )
+      throw new Error( 'publish name should be string!' );
+
+    let thingId = this.thingId;
+    let topic   = `${ thingId }/${ publishName }/#`;
+
+    this.client.unsubscribe( topic, callback )
+  }
+
+ /**
+  * Function for MQTT publication
+  *
+  * @param {string} publishName - what this thing is publishing
+  * @param {string} payload - publication payload
+  * @return Thing
+  * @example publish(`Hello`, `World! [${i++}]`);
+  */
+  publish(publishName :string, payload :string, callback? :Function) :Thing{
+    this.client.publish(`${this.thingId}/${publishName}`, `${payload}`, callback);
+
+    return this;
+  }
+
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import { THING_ID, USER_NAME, PASSWORD, HOST } from './config';
 import { Thing } from './DDMQ/Thing';
 
-let thing = new Thing.Client (THING_ID, USER_NAME, PASSWORD, HOST);
+// initialize thing-js
+let thing = new Thing (THING_ID, USER_NAME, PASSWORD, HOST);
 
+// subscribe
+let sub = thing.subscribe('$inbox', 1, 2, 4);
+
+// handle on any event
+sub.onAny(( msg ) => {
+  console.log(`received - ${ msg }`);
+});
+
+
+// sample code
 let intervalObject;
 if (!intervalObject) {
   intervalObject = setInterval(sayHello, 1000);
@@ -12,10 +23,3 @@ let i = 0;
 function sayHello() {
    thing.publish(`Hello`, `World! [${i++}]`);
 }
-
-let sub = thing.subscribe("$inbox", 1, 2, 4);
-
-// handle on any event
-sub.on((topic, msg) => {
-  console.log(`received - ${topic}: ${msg}`);
-});
